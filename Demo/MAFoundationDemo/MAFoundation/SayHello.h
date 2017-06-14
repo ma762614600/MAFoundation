be356da8c1831dedb5284fba20ef1a869ca6eb73
mengai <hzma@tairanchina.com>
2017-06-14 18:16:04 +0800

    add file

---
+++ Demo/MAFoundationDemo/MAFoundation/SayHello.h
@@ -0,0 +1,13 @@
+//
+//  SayHello.h
+//  MAFoundationDemo
+//
+//  Created by mengai on 14/06/2017.
+//  Copyright © 2017 mengai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SayHello : NSObject
+
+@end
---
+++ Demo/MAFoundationDemo/MAFoundation/SayHello.m
@@ -0,0 +1,13 @@
+//
+//  SayHello.m
+//  MAFoundationDemo
+//
+//  Created by mengai on 14/06/2017.
+//  Copyright © 2017 mengai. All rights reserved.
+//
+
+#import "SayHello.h"
+
+@implementation SayHello
+
+@end
